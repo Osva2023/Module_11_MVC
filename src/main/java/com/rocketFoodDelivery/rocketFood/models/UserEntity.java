09ca26920cb1ae3edf25712237d8fd7a23eb79af
Osvaldo Muguercia <elosvadel84@gmail.com>
2024-03-20 11:52:39 -0400

    Add new files and update settings.json

---
+++ src/main/java/com/rocketFoodDelivery/rocketFood/models/UserEntity.java
@@ -0,0 +1,35 @@
+package com.rocketFoodDelivery.rocketFood.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+
+    @UpdateTimestamp
+    private LocalDateTime updateOn;
+}
